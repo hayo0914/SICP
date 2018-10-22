9e36c08d028becbd2a67f9f2d25329794bdaad40
hayo0914 <hayo0914@gmail.com>
2018-10-22 19:16:26 +0900

    no comment

--- 1-2.scm
+++ 1-2.scm
@@ -119,7 +119,31 @@
         ((= kinds-of-coins 6) 500)))
 (count-change 200)
 
-;; Example 1.12
+;; Excercise 1.11
+
+;; recursive process
+(define (f n)
+  (if (< n 3)
+    n
+    (+ (f (- n 1))
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3))))))
+(f 20)
+
+(require racket/trace)
+(define (f n)
+  (if (< n 3) n (f-iter 2 1 0 n)))
+(define (f-iter a b c count)
+  (if (< count 3)
+    a
+    (f-iter (+ a (* 2 b) (* 3 c))
+            a
+            b
+            (- count 1))))
+(trace f)
+(f 20)
+
+;; Excercise 1.12
 
 ;; version 1
 (require racket/trace)
