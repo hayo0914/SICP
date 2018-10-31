d4e3ab479887e3896031a6c9a13bf895d8dc7243
hayo0914 <hayo0914@gmail.com>
2018-10-31 15:19:29 +0900

    no comment

--- 1-3.scm
+++ 1-3.scm
@@ -250,4 +250,159 @@
 ;   given: 2
 ; [,bt for context]
 
+;; 1.3.3
+;; half-interval method
+
+(define (average a b) (/ (+ a b) 2))
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let ((test-value (f midpoint)))
+        (cond ((positive? test-value)
+               (search f neg-point midpoint))
+              ((negative? test-value)
+               (search f midpoint pos-point))
+              (else midpoint))))))
+(define (close-enough? x y) (< (abs (- x y)) 0.001))
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else (error "Values are not of opposite sign" a b)))))
+(half-interval-method sin 2.0 4.0)
+
+;; Searching for the root of the equation
+;; x^3 - 2*x - 3 = 0
+;; between 1 and 2
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+;; Finding fixed points(不動点) of functions
+;; f(x) = xを満たすx
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+; We can use this method to approximate fixed point of the cosine function
+(let ((result (fixed-point cos 1.0)))
+  (display (format "result = ~a\n" result))
+  (display (format "cos(result) = ~a\n" (cos result))))
+; result = 0.7390822985224023
+; cos(result) = 0.7390870426953322
+
+;; y = sin y + cos y:
+;; finding y
+(fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+;; searching square root
+;; y^2 = x
+;; y = x/y
+;; Unfortunately, this search does not converge.
+(define (sqrt x)
+  (fixed-point (lambda (y) (/ x y)) 1.0))
+(sqrt 9)
+
+;; fixed version(Average Dumping)
+;; y = 1/2(y + x/y)
+;; This technique is called "average damping"(平均減衰)
+(define (sqrt x)
+  (define (average x y) (/ (+ x y) 2))
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+(sqrt 9)
+; > (sqrt 9)
+; 3.0
+; > (sqrt 35)
+; 5.916079783099616
+
+;; Exercise 1.35
+;; 1 + 1/x
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+;; Approximating the golden ratio(1 : 1.618)
+;; x^2 = x + 1
+;; x = 1 + 1/x
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+;; Exercise 1.36
+;; Average Damping
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess step)
+    (let ((next (f guess)))
+      (display (format "step(~a): ~a\n" step next))
+      (if (close-enough? guess next)
+        next
+        (try next (+ 1 step)))))
+  (try first-guess 1))
+; x^x = 1000
+; x = log(1000)/log(x)
+(fixed-point (lambda (x) 
+               (/ (log 1000.0) (log x))) 2)
+; ...
+; step(30): 4.555517548417651
+; step(31): 4.555547679306398
+; step(32): 4.555527808516254
+; step(33): 4.555540912917957
+; step(34): 4.555532270803653
+; 4.555532270803653
+
+;; Not Average Damping
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess step)
+    (let ((next (f guess)))
+      (display (format "step(~a): ~a\n" step next))
+      (if (close-enough? guess next)
+        next
+        (try next (+ 1 step)))))
+  (try first-guess 1))
+; x^x = 1000
+; x = log(1000)/log(x)
+(define (f)
+  (define (average x y) (/ (+ x y) 2))
+  (fixed-point (lambda (x)
+                 (average (/ (log 1000.0) (log x)) x)) 2))
+(f)
+; step(1): 5.9828921423310435
+; step(2): 4.922168721308343
+; step(3): 4.628224318195455
+; step(4): 4.568346513136242
+; step(5): 4.5577305909237005
+; step(6): 4.555909809045131
+; step(7): 4.555599411610624
+; step(8): 4.5555465521473675
+; step(9): 4.555537551999825
+; 4.555537551999825
+
+;; Exercise 1.37
+
+
+
+
+
 
