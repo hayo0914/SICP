1df4576ce131f9525f31e8b2e1f713366b841dbc
hayo0914 <hayo0914@gmail.com>
2018-11-08 18:10:32 +0900

    no comment

--- 2_2.scm
+++ 2_2.scm
@@ -489,7 +489,40 @@
     0
     (map salary
          (filter programmer? records))))
-(salary-of-highest-paid-programmer
+
+; Ex 2.33
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+; Usage
+(display (filter odd? (list 1 2 3 4 5)))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+; Usage
+(accumulate + 0 (list 1 2 3 4 5))
+
+(define (map p sequence)
+  (accumulate
+    (lambda (x y)
+      (cons (p x) y))
+      nil sequence))
+; Usage
+(display (map (lambda (x) (* x x)) (list 1 2 3 4 5 6)))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+; Usage
+(display (append (list 1 2 3) (list 4 5 6)))
+
+
 
 
 
