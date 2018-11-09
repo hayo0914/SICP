d6abc02555c1d802343c043ca9317c34a3362818
hayo0914 <hayo0914@gmail.com>
2018-11-09 17:18:31 +0900

    no comment

--- 2_2.scm
+++ 2_2.scm
@@ -739,3 +739,73 @@
     (lambda (x y) (cons y x))
     nil sequene))
 (display (reverse (list 1 2 3 4 5)))
+
+; Nested Mappings
+(define (enumerate-interval low high)
+  (if (> low high)
+    nil
+    (cons low (enumerate-interval (+ low 1) high))))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
+; Here is a way to generate the sequence of pairs:
+(define (search-sum-prime n)
+  (accumulate
+    append
+    nil
+    (map (lambda (i)
+           (map (lambda (j) (list i j))
+                (enumerate-interval 1 (- i 1))))
+      (enumerate-interval 1 n))))
+; Usage
+(display (search-sum-prime 5))
+; ((2 1) (3 1) (3 2) (4 1) (4 2) (4 3) (5 1) (5 2) (5 3) (5 4))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+; Usage
+(display
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 5)))
+; ((2 1) (3 1) (3 2) (4 1) (4 2) (4 3) (5 1) (5 2) (5 3) (5 4))
+
+(define (find-smallest-divisor n divisor)
+  (cond ((< n (* divisor divisor)) n)
+        ((= 0 (remainder n divisor))
+         divisor)
+        (else
+          (find-smallest-divisor
+            n (+ 1 divisor)))))
+(define (prime? x)
+  (= x (find-smallest-divisor x 2)))
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair)
+        (cadr pair)
+        (+ (car pair) (cadr pair))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+; Final procedure
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+    (filter prime-sum?
+      (flatmap (lambda (i)
+                 (map (lambda (j) (list i j))
+                      (enumerate-interval 1 (- i 1))))
+               (enumerate-interval 1 n)))))
+(display (prime-sum-pairs 5))
+;((2 1 3) (3 2 5) (4 1 5) (4 3 7) (5 2 7))
+
