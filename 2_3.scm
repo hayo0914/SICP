e5ab1f931f574445d354f91df97d507ac29b69a8
hayo0914 <hayo0914@gmail.com>
2018-11-12 15:27:14 +0900

    no comment

--- 2_3.scm
+++ 2_3.scm
@@ -133,4 +133,80 @@
 
 ; Skip Ex 2.56 ~ Ex 2.58 for saving time
 
+; 2.3.3 Example: Representing Sets
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+; Ex 2.59
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (union-set (cdr set1)
+                         (cons (car set1) set2)))))
+(display (union-set '(a b c e) '(b c d f e)))
+
+; Ex 2.60
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+    '()
+    (let ((x1 (car set1)) (x2 (car set2)))
+      (cond ((= x1 x2)
+             (cons x1
+                   (intersection-set (cdr set1)
+                                     (cdr set2))))
+            ((< x1 x2)
+             (intersection-set (cdr set1)
+                               set2))
+            ((< x2 x1)
+             (intersection-set set1 (cdr set2)))))))
+
+(display (intersection-set '(1 2 3) '(2 3 4)))
+
+; Ex 2.61
+(define (adjoin-set x set)
+  (cond ((or (null? set) (< x (car set)))
+         (cons x set))
+        ((= x (car set))
+         set)
+        (else
+          (cons (car set)
+                (adjoin-set x (cdr set))))))
+(display (adjoin-set 5 '(1 3 4 7 8 10)))
+
+; Ex 2.62
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((< (car s2) (car s1))
+         (cons (car s2) (union-set s1 (cdr s2))))
+        ((= (car s2) (car s1))
+         (union-set s1 (cdr s2)))
+        (else
+         (cons (car s1) (union-set (cdr s1) s2)))))
+(display (union-set '(1 2 3 5 7 9) '(0 4 7 10)))
+
+
+
 
