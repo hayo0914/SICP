951b898b8bb8f8468cb3f679a42856b5d17aac53
hayo0914 <hayo0914@gmail.com>
2018-11-13 20:17:09 +0900

    no comment

---
+++ 3_2.scm
@@ -0,0 +1,19 @@
+; 3.2 The Environment Model of Evaluation
+
+; In the presence of assignment, a variable can no longer
+; be cosidered to be merely a name for a value.
+
+; In structure, called "environments", a variable 
+; designate a place in which values can be stored.
+
+; Indeed, one could say that expressions in a
+; programming language do not have any meaning.
+; Rather, an expressions acquires a meaning
+; only with respect to some environment in which
+; it is evaluated.
+
+; 3.2.1 The Rules for Evaluation
+
+; 3.2.2 Applying Simple Procedures
+
+
